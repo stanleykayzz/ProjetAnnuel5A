bbfefb94bbeff5d8b8b15e7b62d1c54e5c93a372
maximem75 <mollard.maxime75@hotmail.fr>
2017-06-25 16:55:31 +0200

    [FRONT] Add confirmation code to client

--- src/main/java/server/mail/EmailServiceImpl.java
+++ src/main/java/server/mail/EmailServiceImpl.java
@@ -6,7 +6,9 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
-
+/**
+ * Created by molla on 22/06/2017.
+ */
 @Component
 public class EmailServiceImpl implements EmailService {
 
