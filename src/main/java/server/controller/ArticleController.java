0c6b4a43e6f0e3fe89bbcdeefc731d56dae10107
ileossa <vincnet.iam@gmail.com>
2017-07-09 13:47:52 +0200

    Add article service and controller

---
+++ src/main/java/server/controller/ArticleController.java
@@ -0,0 +1,76 @@
+package server.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import server.model.Article;
+import server.repository.ArticleRepository;
+import server.repository.ClientRepository;
+import server.service.ClientService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+/**
+ * Created by ileossa on 09/07/2017.
+ */
+@RequestMapping("/api/article")
+public class ArticleController {
+
+    private ArticleRepository articleRepository;
+    private ClientRepository clientRepository;
+    private ClientService clientService;
+
+    @Autowired
+    public ArticleController(ArticleRepository articleRepository, ClientRepository clientRepository, ClientService clientService) {
+        this.articleRepository = articleRepository;
+        this.clientRepository = clientRepository;
+        this.clientService = clientService;
+    }
+
+    @RequestMapping(method = POST)
+    @ResponseStatus(value = OK)
+    public void newArticle(@RequestParam(value="token") String tokenClient,
+                           @RequestBody Article article){
+        if(clientService.isAuthorized(tokenClient)) {
+            articleRepository.saveAndFlush(article);
+        }
+    }
+
+
+    @RequestMapping(method = PUT)
+    @ResponseStatus(value = OK)
+    public void  updateArticle(@RequestParam(value="token")String tokenClient,
+                               @RequestBody Article article){
+        if(clientService.isAuthorized(tokenClient)) {
+            if(articleRepository.findAllByIdArticleExists(article.getIdArticle())){
+                articleRepository.saveAndFlush(article);
+            }
+
+        }
+    }
+
+
+    @RequestMapping(method = DELETE)
+    @ResponseStatus(value = OK)
+    public void deleteArticle(@RequestParam(value="token")String tokenClient,
+                              @RequestParam(value = "id") int articleId){
+        if(clientService.isAuthorized(tokenClient)) {
+            if(articleRepository.findAllByIdArticleExists(articleId)){
+                articleRepository.delete(articleId);
+            }
+        }
+    }
+
+
+    @RequestMapping(method = GET)
+    @ResponseStatus(value = OK)
+    public List<Article> getList(@RequestParam(value = "token")String tokenClient){
+        if(clientService.isAuthorized(tokenClient)) {
+            return articleRepository.findAll();
+        }
+        return new ArrayList<Article>();
+    }
+}
