6cc99b158ca2fb329273b788d2ca9d0776662acf
ileossa <vincnet.iam@gmail.com>
2017-07-10 20:07:48 +0200

    update get rate from newsletter/client after book

--- src/main/java/server/controller/MailController.java
+++ src/main/java/server/controller/MailController.java
@@ -4,14 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import server.model.Booking;
 import server.model.Client;
 import server.model.NewsLetter;
 import server.repository.BookingRepository;
 import server.repository.ClientRepository;
 import server.repository.NewsLetterRepository;
-
-import java.awt.print.Book;
-import java.util.List;
 
 /**
  * Created by ileossa on 03/07/2017.
@@ -35,17 +33,21 @@ public class MailController {
 
     @RequestMapping(value = "/unsubscribe/${email}")
     public String unsucribe(@PathVariable String email){
-        List<Client> client = clientRepository.findClientByEmailEquals(email);
-        NewsLetter letter = newsLetterRepository.findNewsLetterByIdClientEquals(client.get(0).getClientId());
+        Client client = clientRepository.findClientByEmailEquals(email);
+        NewsLetter letter = newsLetterRepository.findNewsLetterByIdClientEquals(client.getClientId());
         letter.setSendNewsLetter(false);
         newsLetterRepository.saveAndFlush(letter);
         return "Description to the newsletter has been well recorded. The cancellation treatment may take a few days.";
     }
 
     @RequestMapping(value = "/rate/${email}/${nb}")
-    public void getRateByClient(@PathVariable String email,
-                                @PathVariable int nb){
-        //todo faire le service
+    public String getRateByClient(@PathVariable String email,
+                                  @PathVariable int nb){
+        Client client = clientRepository.findClientByEmailEquals(email);
+        Booking book = bookingRepository.findBookingByIdClient(client.getClientId());
+        book.setRate(nb);
+        bookingRepository.saveAndFlush(book);
+        return "redirect:index.html";
     }
 
 }
